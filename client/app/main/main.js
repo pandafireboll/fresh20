6d9d82a965dc2028062d2f01bb60cb6d0c998b32
Kai Hsia <kaihsia@gmail.com>
2015-04-11 12:08:55 -0700

    initial

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('fresh2oApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });

7da630c7916ea4f6dfda7fa879ee43ade38184dd
Kai Hsia <kaihsia@gmail.com>
2015-04-11 17:07:16 -0700

    start fresh

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://fresh2o:fresh2o@ds061701.mongolab.com:61701/spaceapps'
+  },
+
+  seedDB: true
+};
